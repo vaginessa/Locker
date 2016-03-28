83d2c1634b2c48315167a3652685a480fc0365f3
Zyg0te <edvard.holst@gmail.com>
2016-03-28 16:26:45 +0200

    Removed unused import

--- android/app/src/main/java/net/zygotelabs/locker/MainFragment.java
+++ android/app/src/main/java/net/zygotelabs/locker/MainFragment.java
@@ -10,7 +10,6 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
