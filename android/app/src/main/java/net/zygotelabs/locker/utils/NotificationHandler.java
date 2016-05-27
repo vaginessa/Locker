61adf33fab7f2b45d2ec157d17f79fe419ff6304
Zygote <edvard.holst@gmail.com>
2016-05-27 12:42:11 +0200

    Added notification helper class and notification icon.
    - Notification used to notify user when wipe condition is triggered.
    
    References #26

---
+++ android/app/src/main/java/net/zygotelabs/locker/utils/NotificationHandler.java
@@ -0,0 +1,39 @@
+package net.zygotelabs.locker.utils;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import net.zygotelabs.locker.MainActivity;
+import net.zygotelabs.locker.R;
+
+/**
+ * Created by Zygote on 27.05.2016.
+ */
+public class NotificationHandler {
+
+    public void createTestModeWipeNotification(Context context){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_notification)
+                        .setContentTitle(context.getString(R.string.notification_safe_mode_wipe))
+                        .setContentText(context.getString(R.string.notification_safe_mode_wipe_summary));
+        Intent resultIntent = new Intent(context, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(123, mBuilder.build());
+    }
+}
